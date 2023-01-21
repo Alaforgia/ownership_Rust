1b41ef02004c82058f1670bb572e35c8f9585f46
Tony LaForgia <alforgia428@gmail.com>
2023-01-20 19:52:43 -0600

    mutable references

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
+// Mutable References
+fn main() {
+    // You can have only one mutable reference to a particular piece of data in a particular scope. 
+    let mut s = String::from("hello");
 
+    change(&mut s);
+}
 
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
 
 
 
